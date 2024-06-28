c46809c1741dad53f1993a80b18ec54413ee260c
Shubhojit Official <shubhojitghosh.005@gmail.com>
2024-06-29 01:29:56 +0530

    added router support

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+
 import Navbar from "./Components/Navbar/Navbar"
 import HomePage from "./pages/HomePage"
 import Footer from "./Components/Footer/Footer"
+import LoginPage from "./pages/LoginPage"
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage/>,
+  },
+  {
+    path: '/login',
+    element: <LoginPage/>
+  }
+])
+
 function App() {
 
   return (
     <>
       <Navbar/>
-      <HomePage/>
-      <Footer/>
+      <RouterProvider router={router}/>
+      {/* <Footer/> */}
     </>
-  )
+
+  //   <>
+  //     <Navbar/>
+  //     <HomePage/>
+  //     <Footer/>
+  //   </>
+   )
 }
 
 export default App
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,19 @@
+function LoginPage(){
+
+    return(
+        <div className="flex flex-col bg-white text-black p-6 pt-2 rounded-sm shadow-[5px_5px_rgba(113,_128,_150,_0.4),_10px_10px_rgba(113,_128,_150,_0.3),_15px_15px_rgba(113,_128,_150,_0.2),_20px_20px_rgba(113,_128,_150,_0.1),_25px_25px_rgba(113,_128,_150,_0.05)]
+             w-96 max-w-md mb-10 md:mb-0">
+
+                <div>
+                    <h1>Pool My Ride</h1>
+                </div>
+            
+
+
+
+
+        </div>
+    )
+}
+
+export default LoginPage;
