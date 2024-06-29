c0ce3b7050298a15324cf8e242ecdc5e21445d09
Shubhojit Official <shubhojitghosh.005@gmail.com>
2024-06-29 13:06:13 +0530

    added users endpoint

--- server/server.py
+++ server/server.py
@@ -1,16 +1,54 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
 
 app = Flask(__name__)
 CORS(app)
 
+cred = credentials.Certificate("../credentials.json")
+firebase_admin.initialize_app(cred)
 
-@app.route("/api/test-data", methods=['GET'])
-def get_data():
-    data = {"message": "sending stuff from flask"}
-    return jsonify(data)
+db = firestore.client()
 
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    try:
+        data = request.get_json()
+        
+        full_name = data['fullName']
+        age = data['age']
+        email = data['email']
+        password = data['pass']
+        is_driver = data['isDriver']
+        driving_xp = data['drivingXp']
+        license_image = data['licenseImage']
+        vehicle_type = data['vehicleType']
+        vehicle_number = data['vehicleNumber']
+        
+        # Add user data to Firestore
+        user_ref = db.collection('users').document(email)
+        user_ref.set({
+            'fullName': full_name,
+            'age': age,
+            'email': email,
+            'pass': password,
+            'isDriver': is_driver,
+            'drivingXp': driving_xp,
+            'licenseImage': license_image,
+            'vehicleType': vehicle_type,
+            'vehicleNumber': vehicle_number
+        })
+        
+        return jsonify({'status': 'success', 'message': 'User added successfully'}), 201
+    
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)}), 500
+        
+        
 
 if __name__ == "__main__":
     app.run(debug=True)
