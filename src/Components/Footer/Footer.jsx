4b5e1aa30f11907971b5c060e4b502cef1404b7e
SANJOY <paulsanjoy2923@gmail.com>
2024-07-08 15:04:57 +0530

    Homepage updated, Responsive for phone feature added, made navbar from scratch for mobile and its workable

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,31 +1,31 @@
 function Footer() {
     return (
-        <div>
+        <div className="max-sm:mt-20">
             <div className=" bg-[#FBFCF5] text-black flex justify-around py-10">
-                <div className="flex flex-col justify-center">
+                <div className="flex flex-col justify-center max-sm:hidden">
                     <div><a href="#"><img src="./src/assets/CAR.png" alt="" /></a></div>
                     <p className="text-black text-5xl font-bold mt-5 pl-8">PoolMyRide</p>
                 </div>
-                <div className="flex gap-48 font-fontMont">
-                    <div className="flex flex-col justify-center items-center gap-10">
-                        <h3 className="font-bold text-2xl">Community</h3>
-                        <ul className=" flex flex-col gap-10 text-base">
+                <div className="flex gap-48 font-fontMont max-sm:gap-12">
+                    <div className="flex flex-col justify-center items-center gap-10 max-sm:gap-5">
+                        <h3 className="font-bold text-2xl max-sm:text-xl">Community</h3>
+                        <ul className=" flex flex-col gap-10 text-base max-sm:gap-4 max-sm:text-sm">
                             <li><a href="#">Contributors</a></li>
                             <li><a href="#">The Portal</a></li>
                             <li><a href="#">Live Events</a></li>
                         </ul>
                     </div>
-                    <div className="flex flex-col justify-center items-center gap-10">
-                        <h3 className="font-bold text-2xl">Company</h3>
-                        <ul className="flex flex-col gap-10 text-base">
+                    <div className="flex flex-col justify-center items-center gap-10 max-sm:gap-5">
+                        <h3 className="font-bold text-2xl max-sm:text-xl">Company</h3>
+                        <ul className="flex flex-col gap-10 text-base max-sm:gap-4 max-sm:text-sm">
                             <li><a href="#">About Us</a></li>
                             <li><a href="#">Contact Us</a></li>
                             <li><a href="#">History</a></li>
                         </ul>
                     </div>
-                    <div className="flex flex-col justify-center items-center gap-10">
-                        <h3 className="font-bold text-2xl">Socials</h3>
-                        <ul className="flex flex-col gap-10 text-base">
+                    <div className="flex flex-col justify-center items-center gap-10 max-sm:gap-5">
+                        <h3 className="font-bold text-2xl max-sm:text-xl">Socials</h3>
+                        <ul className="flex flex-col gap-10 text-base max-sm:gap-4 max-sm:text-sm">
                             <li><a href="#">Twitter</a></li>
                             <li><a href="#">Instagram</a></li>
                             <li><a href="#">Github</a></li>
@@ -33,7 +33,7 @@ function Footer() {
                     </div>
                 </div>
             </div>
-            <div className="bg-[#E9ECDB] text-black font-fontMont text-sm py-7 flex justify-around">
+            <div className="bg-[#E9ECDB] text-black font-fontMont text-sm py-7 flex justify-around max-sm:flex-col max-sm:items-center  max-sm:py-3">
                 <p>Copyright © Men of Culture 2024-2027</p>
                 <p>Made with ♥ on Github</p>
             </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,12 +5,12 @@ function HomePage(){
     return(
         <>
             <title>PoolMyRide</title>
-            <div className="h-screen flex gap-x-80 flex-row items-center justify-center">
+            <div className="xl:h-screen flex xl:gap-x-80 flex-row items-center justify-center max-sm:gap-20 max-sm:w-screen max-sm:flex-col-reverse max-sm:justify-end max-sm:mt-20">
                 <PrimaryCard/>
                 <div className="text-center">
-                    <h2 className="text-6xl leading-snug font-bold">Pooling Made Easy</h2>
-                    <h2 className="text-6xl leading-snug font-bold">And</h2>
-                    <h2 className="text-6xl leading-snug font-bold">Affordable</h2>
+                    <h2 className="text-6xl leading-snug font-bold max-sm:text-4xl">Pooling Made Easy</h2>
+                    <h2 className="text-6xl leading-snug font-bold max-sm:text-4xl">And</h2>
+                    <h2 className="text-6xl leading-snug font-bold max-sm:text-4xl">Affordable</h2>
                 </div>
             </div>
     
