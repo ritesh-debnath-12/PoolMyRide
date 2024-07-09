bbd7fc5a92cac1541b2e0ad736d42f2792bc394a
SANJOY <paulsanjoy2923@gmail.com>
2024-07-09 20:48:16 +0530

    updated the footer width little bit it was making some problems for thin mobiles

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
                     <div><a href="#"><img src="./src/assets/CAR.png" alt="" /></a></div>
                     <p className="text-black text-5xl font-bold mt-5 pl-8">PoolMyRide</p>
                 </div>
-                <div className="flex gap-48 font-fontMont max-sm:gap-12">
+                <div className="flex gap-48 font-fontMont max-sm:gap-10">
                     <div className="flex flex-col justify-center items-center gap-10 max-sm:gap-5">
                         <h3 className="font-bold text-2xl max-sm:text-xl">Community</h3>
                         <ul className=" flex flex-col gap-10 text-base max-sm:gap-4 max-sm:text-sm">
