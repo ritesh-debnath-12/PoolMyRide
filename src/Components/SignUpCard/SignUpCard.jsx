312ccf62a8af99758020fea3ab1a373d354a076d
SANJOY <paulsanjoy2923@gmail.com>
2024-07-12 16:06:41 +0530

    Sign Up page is perfect fom mobile and pc now

--- src/Components/SignUpCard/SignUpCard.jsx
+++ src/Components/SignUpCard/SignUpCard.jsx
@@ -43,7 +43,7 @@ function SignUpCard() {
   };
 
   return (
-    <div className="flex items-center justify-center xl:min-h-screen bg-background mt-14">
+    <div className="flex items-start justify-center xl:min-h-screen bg-background mt-14 mb-14">
       <title>SignUp</title>
       <div className="rounded-lg border bg-card text-card-foreground shadow-sm w-full max-w-md p-6 sm:p-8 max-sm:w-[90vw]">
         <div className="flex flex-col space-y-1.5 p-6">
@@ -54,7 +54,7 @@ function SignUpCard() {
             Please provide the following information to register as a rider.
           </p>
         </div>
-        <div className="p-6 space-y-4 max-sm:p-1">
+        <div className="p-1 space-y-4 max-sm:p-1">
           <div className="grid grid-cols-2 gap-4">
             <div className="space-y-2">
               <label
@@ -146,7 +146,7 @@ function SignUpCard() {
         </div>
         {isDriver && <DriverReg handleDriverChange={handleDriverChange} />}
         <button
-          className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 w-full transition-all duration-300 ease-in-out bg-transparent border-2 border-gray-600 border-solid cursor-pointer select-none hover:text-white hover:border-white focus:shadow-xs focus:no-underline max-sm:mt-10"
+          className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 w-full transition-all duration-300 ease-in-out bg-transparent border-2 border-gray-600 border-solid cursor-pointer select-none hover:text-white hover:border-white focus:shadow-xs focus:no-underline max-sm:mt-10 mt-5"
           type="submit"
           onClick={onRegister}
         >
